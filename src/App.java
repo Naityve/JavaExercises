58e55d9b8530c984610fb1eb454ad23aa922a592
Naityve <longinflorek@gmail.com>
2022-04-12 20:33:01 +0100

    added exampleMath()

--- src/App.java
+++ src/App.java
@@ -1,9 +1,11 @@
 import java.util.Scanner;
 import javax.swing.JOptionPane;
+import java.math.*;
+
 
 public class App {
     public static void main(String[] args) throws Exception {
-        exampleGUI();
+        exampleMath();
     }
 
     public static void exampleScanner() {
@@ -24,5 +26,29 @@ public class App {
         int age = Integer.parseInt(JOptionPane.showInputDialog("Enter your age"));
 
         JOptionPane.showMessageDialog(null, "You are " + age + " years old.");
+    }
+
+    public static void exampleMath() {
+        double x = 3.14;
+        double y = 16;
+
+        double z = Math.max(x,y);
+        double q = Math.sqrt(y);
+        double r = Math.round(x);
+        double c = Math.ceil(x); // floor() rounds down
+
+        System.out.println(z);
+        System.out.println(q);
+        System.out.println(r);
+        System.out.println(c);
+
+        System.out.print("The hypotenuse of a triangle of sides 12.5 and 7.25 is: ");
+
+        double num1 = 12.5;
+        double num2 = 7.25;
+
+        double answer = Math.sqrt((num1*num1)+(num2*num2));
+
+        System.out.print(answer);
     }
 }
